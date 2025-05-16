29d6edfe73147cefc27c6e3979baef29d9d5da3a
Vo Hoang Long <longvh@amazon.com>
2025-05-16 13:32:47 +0900

    chore: bump plugins version

--- after/ftplugin/java.lua
+++ after/ftplugin/java.lua
@@ -1,5 +1,4 @@
 local jdtls = require("jdtls")
-local mason_registry = require("mason-registry")
 
 local root_files = {
 	".git",
@@ -12,17 +11,17 @@ local root_files = {
 
 local root_dir = vim.fs.root(0, root_files)
 local workspace_dir = vim.fn.stdpath("cache") .. "/nvim-jdtls"
-local path_to_jdtls = mason_registry.get_package("jdtls"):get_install_path()
+local path_to_jdtls = vim.fn.expand("$MASON/packages/jdtls")
 local path_to_lombok = path_to_jdtls .. "/lombok.jar"
 
 local java_bundles = {}
 
-local path_to_java_debug = mason_registry.get_package("java-debug-adapter"):get_install_path()
+local path_to_java_debug = vim.fn.expand("$MASON/packages/java-debug-adapter")
 local paths_to_java_debug_bundles =
 	vim.fn.glob(path_to_java_debug .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true, true)
 vim.list_extend(java_bundles, paths_to_java_debug_bundles)
 
-local path_to_java_test = mason_registry.get_package("java-test"):get_install_path()
+local path_to_java_test = vim.fn.expand("$MASON/packages/java-test")
 local paths_to_java_test_bundles = vim.fn.glob(path_to_java_test .. "/extension/server/*.jar", true, true)
 vim.list_extend(java_bundles, paths_to_java_test_bundles)
 
